58f7bc2bfd7e06d4b5f925af7b1a54d584b1cd93
WorldIT-academy <worldit_academy@worldit.academy>
2025-01-30 21:00:24 +0200

    Modified file_search and added lambda function to a search button

--- modules/gui/app.py
+++ modules/gui/app.py
@@ -52,7 +52,7 @@ class App(ctk.CTk):
             ch_master= self.VERTICAL_MENU, 
             name_icon= 'explorer.png', 
             size= self.VERTICAL_MENU._current_width * 0.5,
-            function= search_image    
+            function= lambda: search_image(parent= self)    
         )
         self.BUTTON.place(x= 0, y= 0)
         
--- modules/tools/file_search.py
+++ modules/tools/file_search.py
@@ -1,14 +1,27 @@
 import tkinter.filedialog as filedialog
 
-def search_image():
+def search_image(parent: object):
+    list_name_files = []
     list_files = filedialog.askopenfilenames(
         title= "Search files images",
         initialdir= "/",
         filetypes= [("image files", ["*.png", "*.jpg", "*.jpeg", "*.ico"])],
-        parent= None
+        parent= parent
     )
     print()
-    for name_file in list_files:
-        print(name_file)
+    for name_file in list_files: 
+        # Example output:
+        # list_files = [
+        #    '/Users/worlditacademy/Downloads/photo-editing_11594650.png',
+        #    '/Users/worlditacademy/Downloads/photo-editing_11594650.ico',
+        #    '/Users/worlditacademy/Downloads/photo-editing_11594650-_1_.ico',
+        #    '/Users/worlditacademy/Downloads/photo-editing_11594650 (1).png',
+        #    '/Users/worlditacademy/Downloads/worldIT_LOGO.ico'
+        # ]
+        # name_file = '/Users/worlditacademy/Downloads/worldIT_LOGO.ico'
+        # name_file.split('/') -> ['', 'Users', 'worlditacademy', 'Downloads', 'worldIT_LOGO.ico']
+        # name_file.split('/')[-1] -> 'worldIT_LOGO.ico'
+        print(name_file.split('/')[-1]) # -> worldIT_LOGO.ico
+        list_name_files.append(name_file.split("/")[-1])
     print()
 
